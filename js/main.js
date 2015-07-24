cf239f5fd9a3c76ab247f61d4fd4dbf6149ad1bb
Andrew Hipp <andrew@havenagency.com>
2015-07-24 13:52:58 -0700

    Miscellaneous updates.

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@
 
 
 /**
- * Global Variables
+ * Store usefull Dom elements
  */
 
 var $doc = 	$(document);
@@ -40,67 +40,62 @@ var $body = $('body');
 
 
 
-$doc.ready(function(){
+/**
+ * GLOBAL PROPERTIES AND METHODS
+ *
+ * [1] Global default timer
+ * [2] Property to test against to determine if an event is occuring.
+ * [3] Method to run a function if no events are happening.
+ * [4] Optional timer parameter to override default.
+ */
 
-    /**
-     * GLOBAL PROPERTIES AND METHODS
-     *
-     * [1] Global default timer
-     * [2] Property to test against to determine if an event is occuring.
-     * [3] Method to run a function if no events are happening.
-     * [4] Optional timer parameter to override default.
-     */
+var global = {
+    timer: 2000, // [1]
+    events: true, // [2]
+    detectEvents: function(functionToRun, timer){ // [3 & 4]
 
-    var global = {
-        timer: 2000, // [1]
-        events: true, // [2]
-        detectEvents: function(functionToRun, timer){ // [3 & 4]
+        var _timer = timer || global.timer; // [4]
 
-            var _timer = timer || global.timer; // [4]
+        if(global.events){
 
-            if(global.events){
+            global.events = false;
 
-                global.events = false;
+            setTimeout(function(){
+                global.events = true;
+            }, _timer);
 
-                setTimeout(function(){
-                    global.events = true;
-                }, _timer);
-
-                functionToRun();
-
-            }
+            functionToRun();
 
         }
-    };
+
+    }
+};
 
 
 
 
 
-    /**
-     * TOGGLE ACTIVE STATE
-     *
-     * Reusable Active State toggle function. To toggle 'is-active' on a different
-     * element add data-target('.class') or data-target('#id')
-     *
-     * [1] Store $this as clicked
-     * [2] Check if data-target is set, and store it as _data
-     * [3] If data-target was set pass it through jQuery and use it as the
-     *     target, or assume the clicked element.
-     */
+/**
+ * TOGGLE ACTIVE STATE
+ *
+ * Reusable Active State toggle function. To toggle 'is-active' on a different
+ * element add data-target('.class') or data-target('#id')
+ *
+ * [1] Store $this as clicked
+ * [2] Check if data-target is set, and store it as _data
+ * [3] If data-target was set pass it through jQuery and use it as the
+ *     target, or assume the clicked element.
+ */
 
-    $(document.body).on('click', '.js-toggle-active', function(e){
+$(document.body).on('click', '.js-toggle-active', function(e){
 
-        e.preventDefault();
+    e.preventDefault();
 
-        var _this =  $(this); // [1]
-        var data =   _this.data('target'); // [2]
+    var _this =  $(this); // [1]
+    var data =   _this.data('target'); // [2]
 
-        var target = data ? $(data) : _this; // [3]
+    var target = data ? $(data) : _this; // [3]
 
-            target.toggleClass('is-active');
-
-    });
-
+        target.toggleClass('is-active');
 
 });
